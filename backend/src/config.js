31faa59ed21e1bd33ff02144b0c601bc9e06e730
cryptoc11 <chrislawrie@mail.co.uk>
2022-04-16 13:40:31 +0100

    aaa

--- backend/src/config.js
+++ backend/src/config.js
@@ -2241,7 +2241,7 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTO ';
+const CONTRACT_NAME = 'CRYPTO';
 const CONTRACT_SYMBOL = 'Cp';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x121c742B7BE716C0308621f7c385C5DA0FF3a681';
@@ -2254,7 +2254,7 @@ const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-03-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE =null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x121c742B7BE716C0308621f7c385C5DA0FF3a681"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
