d82a64bd319d2a172097edfba48fb1a8a55ca5d8
cryptoc11 <chrislawrie@mail.co.uk>
2022-04-19 17:02:32 +0100

    j

--- backend/src/config.js
+++ backend/src/config.js
@@ -2254,7 +2254,7 @@ const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2021-04-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2021-03-01T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2021-06-01T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x121c742B7BE716C0308621f7c385C5DA0FF3a681"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
